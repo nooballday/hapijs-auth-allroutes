01388219f9e4efc741aa90193c55dfa5b964cee0
Achmad Naufal <achmadnaufalsyafiq@gmail.com>
2017-10-12 12:00:49 +0700

    number commits

--- validate.js
+++ validate.js
@@ -3,6 +3,7 @@ var model_user = require('./model/index');
 //validate function by comparing username inside payload and model username
 module.exports = (decoded, request, callback) => {
     console.log(request.params.username);
+    //version 1.0.2
     if(request.params.username == decoded.username){
         return callback(null, true);
     } else {
