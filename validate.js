1e8b0520b6e70de130b116c291d8ae9f27a7602b
Achmad Naufal <achmadnaufalsyafiq@gmail.com>
2017-10-12 12:15:06 +0700

    adding some more features

--- validate.js
+++ validate.js
@@ -5,6 +5,7 @@ module.exports = (decoded, request, callback) => {
     console.log(request.params.username);
     //version 1.0.2
     //adding some features
+    //adding some more features
     if(request.params.username == decoded.username){
         return callback(null, true);
     } else {
